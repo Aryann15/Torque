9403b359b94cff5ab0e35d0511bdc20c3bf3cb9a
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-30 02:16:57 +0530

    upload csv

--- src/components/Csv_bot.jsx
+++ src/components/Csv_bot.jsx
@@ -2,23 +2,53 @@ import React from "react";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
+import Container from "@mui/material/Container";
+import { useState } from "react";
 
 const Csvbot = () => {
+
+const [isFileUploaded, setIsFileUploaded] = useState(false)
+
+
+function handleFileUpload (event) {
+  setIsFileUploaded(true)
+}
   return (
-    <>
-      <h1>CSV BOT</h1>
-      <center>
-        <Card variant="outlined" style={{ width: 400, padding: 20 }}>
-          <Typography variant="h5">Ask about your CSV</Typography>
-          <br /><br />
-          <Button variant="contained" component="label">
-            Upload csv
-            <input type="file" hidden />
-          </Button><br /><br />
     
+      <Container>
+        <Typography variant="h3" align="center" gutterBottom>
+          CSV BOT
+        </Typography>
+        <Card variant="outlined" sx={{ maxWidth: 400, p: 3, mx: "auto" }}>
+          <Typography variant="h5">Ask about your CSV</Typography>
+          <br />
+          <br />
+          <label htmlFor="csv-upload">
+            <Button variant="contained" component="span">
+              Upload CSV
+            </Button>
+            <input
+              type="file"
+              id="csv-upload"
+              accept=".csv"
+              style={{ display: "none" }}
+              onChange={handleFileUpload}
+            />
+
+          </label>
+          {isFileUploaded ? (
+        <div>
+          <p>File uploaded successfully!</p>
+          {/* You can add more content or actions here */}
+        </div>
+      ) : (
+        <div>
+          <p>Upload your CSV file</p>
+          {/* Render your file upload input as before */}
+        </div>)}
         </Card>
-      </center>
-    </>
+      </Container>
+    
   );
 };
 
