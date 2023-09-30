4e42ad260044525d6ea0f4e8f7dc2c08c6b94876
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-30 13:44:03 +0530

    take user question

--- src/components/Csv_bot.jsx
+++ src/components/Csv_bot.jsx
@@ -4,51 +4,58 @@ import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
 import Container from "@mui/material/Container";
 import { useState } from "react";
+import TextField from "@mui/material/TextField";
 
 const Csvbot = () => {
+  const [userCsv, setUserCsv] = useState(null);
+  const [userQuestion, setUserQuestion] = useState(null)
 
-const [isFileUploaded, setIsFileUploaded] = useState(false)
-
-
-function handleFileUpload (event) {
-  setIsFileUploaded(true)
-}
+  function handleFileUpload(event) {
+    setUserCsv(event.target.files[0]);
+  }
+  function handleQuestionUpload(event) {
+    setUserQuestion(event.target.value);
+  }
   return (
-    
-      <Container>
-        <Typography variant="h3" align="center" gutterBottom>
-          CSV BOT
-        </Typography>
-        <Card variant="outlined" sx={{ maxWidth: 400, p: 3, mx: "auto" }}>
-          <Typography variant="h5">Ask about your CSV</Typography>
-          <br />
-          <br />
-          <label htmlFor="csv-upload">
-            <Button variant="contained" component="span">
-              Upload CSV
-            </Button>
-            <input
-              type="file"
-              id="csv-upload"
-              accept=".csv"
-              style={{ display: "none" }}
-              onChange={handleFileUpload}
-            />
-
-          </label>
-          {isFileUploaded ? (
-        <div>
-          <p>File uploaded successfully!</p>
-          {/* You can add more content or actions here */}
-        </div>
-      ) : (
-        <div>
-          <p>Upload your CSV file</p>
-          {/* Render your file upload input as before */}
-        </div>)}
-        </Card>
-      </Container>
-    
+    <Container>
+      <Typography variant="h3" align="center" gutterBottom>
+        CSV BOT
+      </Typography>
+      <Card variant="outlined" sx={{ maxWidth: 400, p: 3, mx: "auto" }}>
+        <Typography variant="h5">Ask about your CSV</Typography>
+        <br />
+        <br />
+        <label htmlFor="csv-upload">
+          <Button variant="contained" component="span">
+            Upload CSV
+          </Button>
+          <input
+            type="file"
+            id="csv-upload"
+            accept=".csv"
+            style={{ display: "none" }}
+            onChange={handleFileUpload}
+          />
+          {userCsv ? (
+            <div>
+              <p>File uploaded successfully!</p>
+              {/* {console.log(userCsv)} */}
+              <TextField
+                id="outlined-basic"
+                label="Ask about your csv"
+                variant="outlined"
+                onChange={handleQuestionUpload}
+              />
+              {console.log(userQuestion)}
+            </div>
+          ) : (
+            <div>
+              <p>Upload your CSV file</p>
+            </div>
+          )}
+        </label>
+      </Card>
+    </Container>
   );
 };
 
