c1785a371eda16319d83c533c673995900dc097c
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-01 11:36:57 +0530

    parse csv

--- bots/venv/csv_bot.py
+++ bots/venv/csv_bot.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from langchain.agents import create_csv_agents
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+
+@app.route('/parse-csv', methods=['POST'])
+def parse_csv():
+      csv_file = request.files['csv']
+      csv_file.save('uploaded.csv')
+      return 'CSV uploaded'
+
+
+
+load_dotenv()
