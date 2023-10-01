524778d5ee46e7b8f489bf0e54fe31870c80ae2a
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-01 15:22:21 +0530

    new file structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Csvbot from './components/Csv_bot';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Csvbot />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Csv_bot.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+import Container from "@mui/material/Container";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+
+
+
+const Csvbot = () => {
+
+
+  const [userCsv, setUserCsv] = useState(null);
+  const [userQuestion, setUserQuestion] = useState(null)
+  const [answer, setAnswer] = useState(null);
+
+
+  function handleFileUpload(event) {
+    setUserCsv(event.target.files[0]);
+  }
+  function handleQuestionUpload(event) {
+    setUserQuestion(event.target.value);
+  }
+
+
+  return (
+    <Container>
+      <Typography variant="h3" align="center" gutterBottom>
+        CSV BOT
+      </Typography>
+      <Card variant="outlined" sx={{ maxWidth: 400, p: 3, mx: "auto" }}>
+        <Typography variant="h5">Ask about your CSV</Typography>
+        <br />
+        <br />
+        <label htmlFor="csv-upload">
+          <Button variant="contained" component="span">
+            Upload CSV
+          </Button>
+          <input
+            type="file"
+            id="csv-upload"
+            accept=".csv"
+            style={{ display: "none" }}
+            onChange={handleFileUpload}
+          />
+          {userCsv ? (
+            <div>
+              <p>File uploaded successfully!</p>
+              {/* {console.log(userCsv)} */}
+              <TextField
+                id="outlined-basic"
+                label="Ask about your csv"
+                variant="outlined"
+                onChange={handleQuestionUpload}
+              />
+
+              <Button onClick={getAnswer}>
+                get answer
+              </Button>
+              <Typography>{answer}</Typography>
+              {/* {console.log(userQuestion)} */}
+            </div>
+          ) : (
+            <div>
+              <p>Upload your CSV file</p>
+            </div>
+          )}
+        </label>
+      </Card>
+    </Container>
+  );
+};
+
+export default Csvbot;
